b3aa8d8872526b6eea62b98aea34e31c68563894
ma-abasian <abasianmohammadali@gmail.com>
2021-09-18 16:42:48 +0430

    social

--- Js/java.js
+++ Js/java.js
@@ -1,55 +1,76 @@
+// videoPlayer
+// videoPlayer
+// videoPlayer
 
-        // videoPlayer
-        // videoPlayer
-        // videoPlayer
+var videoPlayer = document.getElementById("videoPlayer");
+var myVideo = document.getElementById("myVideo");
 
-        var videoPlayer = document.getElementById("videoPlayer");
-        var myVideo = document.getElementById("myVideo");
+function playPause(file) {
+    myVideo.src = file;
+    videoPlayer.style.display = "block";
+    myVideo.play();
+}
 
-        function playPause(file) {
-            myVideo.src = file;
-            videoPlayer.style.display = "block";
-            myVideo.play();
-        }
+function stopVideo() {
+    myVideo.pause();
+    videoPlayer.style.display = "none";
+}
 
-        function stopVideo() {
-            myVideo.pause();
-            videoPlayer.style.display = "none";
-        }
-
-        // if (myVideo.paused) {
-        //         $('#video-s').show();
-        //         myVideo.play();
-        //     }
-        //     else {
-        //         $('#video-s').hide();
-        //         myVideo.pause();
-        //     }
+// if (myVideo.paused) {
+//         $('#video-s').show();
+//         myVideo.play();
+//     }
+//     else {
+//         $('#video-s').hide();
+//         myVideo.pause();
+//     }
 
 
-// menu
-// menu
+// sub menu
+// sub menu
 
-        var timer = {};
-        var menu = document.getElementById("menu");
+var timer = {};
+var menu = document.getElementById("menu");
 
-        $('> li', menu).hover(function () {
-            var tag = $(this);
-            var timerAttr = tag.attr('data-time');
-            
-            clearTimeout(timer[timerAttr]);
-            timer[timerAttr] = setTimeout(function () {
-                $('> .sub-menu',tag).fadeIn(0);
+$('> li', menu).hover(function () {
+    var tag = $(this);
+    var timerAttr = tag.attr('data-time');
+
+    clearTimeout(timer[timerAttr]);
+    timer[timerAttr] = setTimeout(function () {
+        $('> .sub-menu',tag).fadeIn(0);
                 
-            }, 200);
-        }, function () {
-            var tag = $(this);
-            
-            var timerAttr = tag.attr('data-time');
-            clearTimeout(timer[timerAttr]);
-            
-            timer[timerAttr] = setTimeout(function () {
-                $('> .sub-menu',tag).fadeOut(0);
+    }, 200);
+}, function () {
 
-            }, 200);
-        });
+    var tag = $(this);
+
+    var timerAttr = tag.attr('data-time');
+    clearTimeout(timer[timerAttr]);
+            
+    timer[timerAttr] = setTimeout(function () {
+        $('> .sub-menu',tag).fadeOut(0);
+
+    }, 200);
+});
+
+// footer social
+// footer social
+
+
+var social = document.getElementById("social");
+
+function social_click(tag) {
+
+    if ($(tag).hasClass('active')) {
+        $('a', social).hide();
+        $('span', social).removeClass('active');
+    } else {
+
+        $('a', social).hide();
+        $('span', social).removeClass('active');
+        $('> a', tag).show();
+        $(tag).addClass('active');
+    }
+
+}
